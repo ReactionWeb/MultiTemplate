437571e3271f58be1c62829af1540766494cc302
folenor <themrmegaanton16@gmail.com>
2021-02-07 13:15:18 +0300

    Db connection
    Базовая логика подключения к базе, запуск сервера теперь осуществляется через асинхронную функцию

--- frontend/src/component/Headers/Header1.tsx
+++ frontend/src/component/Headers/Header1.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {AppBar, Box, Toolbar} from "@material-ui/core";
+import {AppBar, Box, Toolbar, Button} from "@material-ui/core";
 
 const Header1 = () => {
     return(
         <Box>
             <AppBar position="fixed">
-                <Toolbar>{/* содержимое */}</Toolbar>
+                <Toolbar>
+                    <Button color="inherit">Вход</Button>
+                </Toolbar>
             </AppBar>
-            <Toolbar />
         </Box>
     )
 }
