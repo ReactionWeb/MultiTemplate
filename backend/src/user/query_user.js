1a80d7a5c5889147fd29d0fcf9bef947d8d9b1a2
folenor <themrmegaanton16@gmail.com>
2021-02-09 21:00:55 +0300

    Заготовка для регистрации и логина
    Пока что заготовка функций, без роутинга. Тестирование и роутинг завтра

--- backend/src/user/query_user.js
+++ backend/src/user/query_user.js
@@ -3,13 +3,41 @@ const { User } = require('../models');
 const { tokenDecode, tokenSign} = require('./webtoken');
 
 const registerUser = async(username, email, password) => {
-    let salt = await bcrypt.genSalt(10);
-    let hash_pass = await bcrypt.hash(password, salt); 
+    let hash_pass = await bcrypt.hash(password, 10); 
     try{
         await User.create({
-
-        })
+            userName: username,
+            email: email,
+            password: hash_pass
+        });
     } catch(e) {
-        
+        throw('Ошибка регистрации');
+    }
+}
+
+const authUser = async(username, password) => {
+    const user = await User.findOne({
+        where: {
+            userName: username
+        },
+    });
+    if (user == null){
+        console.log('Пользователь не найден');
+    }
+    else{
+        const match = await bcrypt.compare(password, user.password);
+
+        if (match){
+            const token = tokenSign({ id: user.id,
+                                       userName: user.userName});
+            const authToken = {
+                userName: username,
+                token: token
+            }
+            return(authToken);
+        }
+        else{
+            throw('Пароль введен неверно');
+        }
     }
 }
